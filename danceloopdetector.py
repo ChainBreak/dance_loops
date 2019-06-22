a74b9944cc30efacdfab18c525ea1932ae868c23
Thomas Rowntree <thomas.james.rowntree@gmail.com>
2019-06-22 23:58:12 +0930

    Just comments

--- danceloopdetector.py
+++ danceloopdetector.py
@@ -7,16 +7,17 @@ import queue
 from filters import HighPass
 
 class DanceLoopDetector():
+    """This class can be called for every frame from a camera and it will return a boolean indicating if someone is dancing"""
 
     def __init__(self,p):
 
-        self.period = p["period"]
+        self.p = p
 
         self.lag_queue = queue.Queue()
 
         self.auto_cor_queue = queue.Queue()
 
-        
+        self.auto_cor_sum = 0.0
 
         self.last_frame = None
 
@@ -24,7 +25,9 @@ class DanceLoopDetector():
 
     
     def calc_motion_ratio(self,frame):
-        #convert frame to int so that it can hold negatives
+        """Calculate the ratio of how much the image has changed since the last image"""
+
+        #convert the frame to int so that it can hold negatives
         frame = frame.astype("int32")
 
         #initialise the last frame if needed
@@ -52,15 +55,18 @@ class DanceLoopDetector():
         #calculate how much motion is in the image
         # 0 = no motion, nothing in the image has changed
         # 1 = full motion, every pixel has changed by full brightness
-        motion = calc_motion_ratio(frame)
+        motion = self.calc_motion_ratio(frame)
 
         #Apply high pass filter to get the relative motion as a zero centered signal
         motion = self.diff_high_pass(motion)
+
+        
 
         #add this motion to the lag queue
         self.lag_queue.put(motion)
         
         #get the 
+        return motion
 
 
 
