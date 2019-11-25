b64b9f66c4d1ad35ce779929e75d366ab0ca8581
Thomas Rowntree <thomas.james.rowntree@gmail.com>
2019-11-26 00:06:01 +1030

    auto-correlate for multiple shifts. find the best shift

--- danceloopdetector.py
+++ danceloopdetector.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import math
 import time
+from scipy.signal import fftconvolve
 from matplotlib import pyplot as plt
 from collections import deque
 from danceloop import DanceLoop
@@ -15,7 +16,7 @@ class DanceLoopDetector():
 
         self.dance_detection_callback = dance_detection_callback
         self.p = p
-
+        print(p)
         self.dance_threshold = p["dance_correlation_threshold"]
         self.dance_std_threshold = p["dance_std_threshold"]
 
@@ -24,7 +25,7 @@ class DanceLoopDetector():
         #number of frames to lag/delay for auto correlation
         self.lag = p["frames_per_loop"] 
 
-        self.window_length = p["frames_per_loop"] * 4
+        self.window_length = 200# p["frames_per_loop"] * 4
 
         self.frame_buffer = deque(maxlen=self.window_length)
 
@@ -33,13 +34,13 @@ class DanceLoopDetector():
         self.last_frame = None
 
         self.last_detect_time = time.time()
-        self.cooldown_time = p["dance_detection_cooldown_time"]
+        self.cooldown_time = 10#p["dance_detection_cooldown_time"]
 
         self.plot_dict = {}
 
     def plot(self,label,value):
-        # self.plot_dict[label] = self.plot_dict.get(label,[])
-        # self.plot_dict[label].append(value)
+        self.plot_dict[label] = self.plot_dict.get(label,[])
+        self.plot_dict[label].append(value)
         pass
 
     def calc_motion_ratio(self,frame):
@@ -70,25 +71,7 @@ class DanceLoopDetector():
         #is effectively the ratio of the image that contained motion
         return diff_ratio
 
-    def calc_lagged_product(self,motion):
-        """An auto correlation is simple a signal multiplied by itself after a delay equal to the period of the frequency you are trying to detect"""
 
-        #add this motion to the lag queue
-        self.lag_buffer.append(motion)
-
-        if len(self.lag_buffer) <= self.lag_length:
-            return 0.0,0.0
-
-        #get the motion from one period ago
-        lagged_motion = self.lag_buffer.popleft()
-
-        #correlate these together
-        lagged_product = motion * lagged_motion
-
-        #The best possible correlation they could have
-        max_lagged_product = max(motion**2,lagged_motion**2)
-
-        return lagged_product,max_lagged_product
 
     def calc_auto_correlation(self, frame):
         
@@ -109,15 +92,54 @@ class DanceLoopDetector():
         #subtract the mean from the buffer so the signal is zero centered
         X = self.motion_buffer - self.motion_buffer.mean()
 
-        #roll the buffer by the lag amount
-        X_lagged = np.roll(X,self.lag)
+        #apply a windowing function to the signal so the ends taper to zero
+        X_windowed = X * np.blackman(self.window_length)
+
+       
+        #calculate the value for correlating the signal with itself for every possible shift
+        correlation = np.correlate(X_windowed,X_windowed,"same")
+
+        #the correlation is symetric so just take the second half
+        correlation = correlation[len(correlation)//2:]
+
+        #the best correlation is the one with zero shift
+        correlation_ratio = correlation / correlation[0] 
+
+        negative_correlations = np.where(correlation_ratio < 0.0)[0]
+        
+        if len(negative_correlations) > 0:
+            zero_crossing_index = negative_correlations[0]
+            
+            best_delay_index = np.argmax(correlation_ratio[zero_crossing_index:]) + zero_crossing_index
+
+            best_correlation_ratio = correlation_ratio[best_delay_index]
+        else:
+            best_delay_index = 0
+            best_correlation_ratio = 0.0
+
+        print(best_correlation_ratio)
+        
+
+
+        if best_correlation_ratio > 0.6: 
+            plt.subplot(2,1,1)
+            plt.plot(X_windowed)
+            plt.plot(np.roll(X_windowed,best_delay_index))
+            plt.plot(X_windowed + np.roll(X_windowed,best_delay_index))
+            plt.subplot(2,1,2)
+            plt.plot(correlation_ratio)
+            
+            
+            plt.show()
 
         #correlate the signal with its lagged version
-        correlation = np.sum(X*X_lagged) / max(np.sum(X**2),10e-6)
+        # correlation = np.sum(X*X_lagged) / max(np.sum(X**2),10e-6)
 
+
+        
         #Only return a correlation is the signal amplitude is above a threshold
         if self.motion_buffer.std() > self.dance_std_threshold:
-            return correlation
+            return 0.0
         else:
             return 0.0
 
@@ -151,28 +173,30 @@ class DanceLoopDetector():
             return
                
 
-        #Use auto correlation to see if the dance frequency is in the video
-        self.correlation = self.calc_auto_correlation(frame)
-        
+        self.calc_auto_correlation(frame)
+
         #there is a x second cooldown after every dance is detected
         cooldown_ended = time.time()-self.last_detect_time > self.cooldown_time
 
-        #if dance frequence detected and cooldown ended then trigger a new dance
-        if self.correlation > self.dance_threshold and cooldown_ended:
-            #reset cooldown timer
-            self.last_detect_time = time.time()
+        # #if dance frequence detected and cooldown ended then trigger a new dance
+        # if self.correlation > self.dance_threshold and cooldown_ended:
+        #     #reset cooldown timer
+        #     self.last_detect_time = time.time()
 
-            loop_frames = self.extract_loop_from_buffer()
+        #     loop_frames = self.extract_loop_from_buffer()
 
-            #construct a dance loop object to wrap these frames
-            dance_loop = DanceLoop(loop_frames)
+        #     #construct a dance loop object to wrap these frames
+        #     dance_loop = DanceLoop(loop_frames)
 
-            self.dance_detection_callback(dance_loop)
-
-            # for label,data in self.plot_dict.items():
-            #     plt.plot(data,label=label)
-            # plt.legend(loc="best")
-            # plt.show()
+        #     self.dance_detection_callback(dance_loop)
+        
+        # if cooldown_ended:
+        #     for label,data in self.plot_dict.items():
+        #         plt.plot(data,label=label)
+                
+        #     plt.legend(loc="best")
+        #     plt.show()
+        #     self.last_detect_time = time.time()
 
 
         
